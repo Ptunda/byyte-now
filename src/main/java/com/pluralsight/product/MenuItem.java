d776a26a3b415374b8e6241b1f6fd8bf019fd2d3
Ptunda <ptunda@pgh.yearup.org>
2024-05-28 21:48:32 -0400

    Create an abstract MenuItem class to provide a blueprint for all menu item classes

---
+++ src/main/java/com/pluralsight/product/MenuItem.java
@@ -0,0 +1,24 @@
+package com.pluralsight.product;
+
+public abstract class MenuItem {
+
+    private String name;
+    private double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public abstract void displayDetails();
+
+}
