732968a894e4582d692fdfc9b7854d5f8e8c76fa
Ptunda <ptunda@pgh.yearup.org>
2024-05-29 13:14:33 -0400

    Added RegularTopping class

---
+++ src/main/java/com/pluralsight/product/RegularTopping.java
@@ -0,0 +1,17 @@
+package com.pluralsight.product;
+
+public class RegularTopping extends Topping {
+
+    public RegularTopping(String name, String toppingType, boolean isExtra) {
+        super(name, toppingType, isExtra);
+    }
+
+    @Override
+    public void displayDetails() {
+
+        System.out.println("For your " + getToppingType() + "s you have.");
+        System.out.println("Name: " + getName());
+        System.out.println("You want extra toppings: " + (isExtra() ? "Yes" : "No"));
+
+    }
+}
