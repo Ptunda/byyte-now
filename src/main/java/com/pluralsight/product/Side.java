bd272d63aab5fb212ead644caebc81cb0564ff04
Ptunda <ptunda@pgh.yearup.org>
2024-05-29 13:25:29 -0400

    Added Side class that extends the RegularTopping class

---
+++ src/main/java/com/pluralsight/product/Side.java
@@ -0,0 +1,19 @@
+package com.pluralsight.product;
+
+public class Side extends RegularTopping{
+
+    public Side(String name, String toppingType, boolean isExtra) {
+
+        super(name, toppingType, isExtra);
+
+    }
+
+
+    @Override
+    public void displayDetails() {
+
+        super.displayDetails();
+
+    }
+
+}
