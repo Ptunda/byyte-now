27a1793bbe3cfc4b63ec3f9b9cb342042156d8eb
Ptunda <ptunda@pgh.yearup.org>
2024-05-30 22:26:33 -0400

    Added the Program class to run my program

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.HomeScreen;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        HomeScreen homeScreen = new HomeScreen();
+        homeScreen.display();
+
+
+    }
+
 }
